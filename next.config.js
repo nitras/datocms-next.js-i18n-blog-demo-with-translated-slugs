cee113d1ba17193504b3ae3638beb212ccd76b62
Peter Eeckelaert <peter@nitras.be>
2023-07-23 23:31:39 +0200

    updates
    updates

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@ require('dotenv').config();
 
 module.exports = {
 	i18n: {
-		locales: ['en', 'nl', 'nl-BE', 'ru'],
-		defaultLocale: 'nl',
+		locales: ['nl-BE', 'en'],
+		defaultLocale: 'nl-BE',
 	},
 	env: {
-		NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:
-			process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
+		NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN: process.env.NEXT_DATOCMS_API_TOKEN,
 	},
 };
