ab145ae9d1e3ce9bba436fff28f9165ae52cbca9
Peter Eeckelaert <peter@nitras.be>
2023-07-24 08:59:35 +0200

    hey
    hey

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,18 @@
 import Alert from '../components/alert';
 import Footer from '../components/footer';
+import { ReactLenis, useLenis } from '@studio-freight/react-lenis';
 
 export default function Layout({ preview, children }) {
+	const lenis = useLenis(({ scroll }) => {
+		// called every scroll
+	});
 	return (
-		<>
+		<ReactLenis root>
 			<div className="min-h-screen">
 				{/* <Alert preview={preview} /> */}
 				<main>{children}</main>
 			</div>
 			<Footer />
-		</>
+		</ReactLenis>
 	);
 }
